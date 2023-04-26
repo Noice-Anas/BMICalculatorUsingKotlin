16eafc561317e5a87942caf5c9bceaebf2899309
Noice_Anas <anas.alhalabi2002@gmail.com>
2023-04-26 10:56:16 +0300

    - Made changes to be perfect for the assignment

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bmicalculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bmicalculator.databinding.ActivityMainBinding
@@ -21,14 +22,9 @@ class MainActivity : AppCompatActivity()
         binding.heightPicker.minValue = 100
         binding.heightPicker.maxValue = 250
 
-        binding.weightPicker.setOnValueChangedListener{ _,_,_ ->
+        binding.buttonCalculate.setOnClickListener {
             calculateBMI()
         }
-
-        binding.heightPicker.setOnValueChangedListener{ _,_,_ ->
-            calculateBMI()
-        }
-
     }
 
     private fun calculateBMI()
@@ -41,19 +37,27 @@ class MainActivity : AppCompatActivity()
         val bmi = weight.toDouble() / (doubleHeight * doubleHeight)
 
         binding.resultsTV.text = String.format("Your BMI is: %.2f", bmi)
+
         binding.healthyTV.text = String.format("Considered: %s", healthyMessage(bmi))
 
     }
 
     private fun healthyMessage(bmi: Double): String
     {
-        if (bmi < 18.5)
+        if (bmi < 18.5) {
+            binding.healthyTV.setTextColor(Color.RED)
             return "Underweight"
-        if(bmi < 25.0)
+        }
+        if(bmi < 25.0) {
+            binding.healthyTV.setTextColor(Color.GREEN)
             return "Healthy"
-        if (bmi < 30.0)
+        }
+        if (bmi < 30.0) {
+            binding.healthyTV.setTextColor(Color.RED)
             return "Overweight"
+        }
 
+        binding.healthyTV.setTextColor(Color.RED)
         return "Obese"
     }
 }
